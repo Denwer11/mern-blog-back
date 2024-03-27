d3a781f16257591324abffd1ec1248f08ba3f64d
Denwer1 <denwer0204@gmail.com>
2024-03-27 21:13:28 +0300

    tags

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -5,6 +5,25 @@ export const getAll = async (req, res) => {
     const posts = await PostModel.find()
       .populate({ path: "user", select: ["name", "avatar"] })
       .exec();
+
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: "Не удалось получить статьи" });
+  }
+};
+
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find()
+      .populate({ path: "user", select: ["name", "avatar"] })
+      .limit(5)
+      .exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
     res.json(posts);
   } catch (err) {
     res.status(500).json({ message: "Не удалось получить статьи" });
